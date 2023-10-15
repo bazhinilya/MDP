dbca4c661c5228d622eba6b0265d751234c21e14
bazhinilya <ilya_bazhin@live.ru>
2023-10-15 19:40:03 +0300

    first commit

---
+++ Graphic.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+plt.rcParams['figure.dpi'] = 150
+
+class graphics:
+    def print(*args, xlabel: str = None, ylabel: str = None, title: str = None, log = False):
+        not_none_params = [item for item in args if item is not None]
+        plt.plot(*not_none_params)
+
+        plt.xlabel(xlabel)
+        if ylabel is not None:
+            plt.ylabel(ylabel)
+            plt.legend(labels=(ylabel), loc='upper left')
+            plt.title(f"Зависимость {ylabel} от {xlabel}")
+        if title is not None: plt.title(title)
+
+        if (log): plt.yscale('log')
+        plt.grid(True)
+        plt.show()
---
+++ MDP.py
@@ -0,0 +1,78 @@
+from Const import const
+from Graphic import graphics as graph
+
+from numpy import array, arange, exp, fabs, sqrt, log
+from scipy.optimize import fsolve
+from math import pi
+from varname import nameof
+
+T_range = arange(const.Tmin, const.Tmax + 1)
+
+#1. Зависимость ширины запрещённой зоны Eg от температуры.
+Eg_T = array(const.Eg0 - (const.alpha_T / (const.beta_T + T_range)) * T_range ** 2)
+graph.print(T_range, Eg_T, xlabel = nameof(T_range), ylabel = nameof(Eg_T))
+
+#2. Зависимость эффективной плотности квантовых состояний Nc, Nv от температуры.
+Nc_T = array(2.5e19 * (const.mn)**(3/2) * (T_range/300)**(3/2)) 
+Nv_T = array(2.5e19 * (const.mp)**(3/2) * (T_range/300)**(3/2))
+graph.print(T_range, Nc_T, T_range, Nv_T, log = True, xlabel = nameof(T_range), ylabel = nameof(Nc_T, Nv_T))
+
+#3. График проверки интеграла Ферми порядка ½.
+eta_range = arange(-10, 9.82, 0.06)
+
+def F_12_eta(eta): 
+    return sqrt(pi/2)**(-1) * (((1.5*2**(1.5))/(2.13+eta+((fabs(eta-2.13))**(1/2.4)+9.6)**(1.5)))+exp(-eta)/(sqrt(pi)/2))**(-1)
+graph.print(eta_range, F_12_eta(eta_range), log = True, xlabel = nameof(eta_range), ylabel = nameof(F_12_eta))
+
+#4. Зависимость положения энергетических уровней на зонной диаграмме от T (Ev, Ec, F, Fi, Ea1, Ea2).
+Ei_T = Eg_T / (const.k * T_range)
+Ev_T = 0 * T_range
+Ec_T = Eg_T
+Fi_T = Ec_T/2 + (3/4) * const.k * T_range * log(const.mp / const.mn)
+
+def Na1_eta(eta, i = None): 
+    if i is None: 
+        return const.Na1 / (1 + 2*exp(-eta + (const.Ea1 - Eg_T) / (const.k * T_range)))    
+    return const.Na1 / (1 + 2*exp(-eta + (const.Ea1 - Eg_T[i]) / (const.k * T_range[i])))
+
+def Na2_eta(eta, i = None): 
+    if i is None:
+        return const.Na2 / (1 + 2*exp(-eta + (const.Ea2 - Eg_T) / (const.k * T_range)))
+    return const.Na2 / (1 + 2*exp(-eta + (const.Ea2 - Eg_T[i]) / (const.k * T_range[i])))
+
+def f_eta_T(eta, i)->float: 
+    na1 = Na1_eta(eta, i)
+    na2 = Na2_eta(eta, i)
+    n = Nc_T[i] * F_12_eta(eta)
+    p = Nv_T[i] * F_12_eta(-eta-Ei_T[i])
+    return p - n - na1 - na2
+
+eta_static = (Fi_T[0] - Eg_T[0]) / (const.k * const.Tmax)
+ETA = [1]
+ETA[0] = fsolve(f_eta_T, eta_static, args = 0, xtol = 1e-12)[0]
+
+for i in range(1, T_range.size):
+    ETA.append(fsolve(f_eta_T, ETA[i-1], args = i, xtol=1e-12)[0])
+
+ETA = array(ETA)
+F_T = ETA * const.k * T_range + Eg_T
+
+graph.print(T_range, Ev_T, T_range, Ec_T, T_range, F_T, T_range, Fi_T, T_range, Ev_T + const.Ea1, T_range, Ev_T + const.Ea2, 
+                 xlabel = nameof(T_range),
+                 ylabel = nameof(Ev_T, Ec_T, F_T, Fi_T) + tuple({"Ea1", "Ea2"}),
+                 title = "Зонная диаграмма")
+
+# 5. Зависимость концентраций электронов n, дырок p, собственной концентрации ni от температуры.
+n_T = Nc_T * F_12_eta(ETA)
+p_T = Nv_T * F_12_eta(-ETA - Eg_T / (const.k * T_range))
+ni_T = sqrt(n_T * p_T)
+graph.print(1/T_range, n_T, 1/T_range, p_T, 1/T_range, ni_T, log = True, xlabel = "1/" + nameof(T_range), ylabel = nameof(n_T, p_T, ni_T))
+
+# 6. Зависимость концентрации ионов доноров и акцепторов для каждой примеси Na1, Na2 от T
+na1 = Na1_eta(ETA)
+na2 = Na2_eta(ETA)
+graph.print(1/T_range, na1, 1/T_range, na2, log = True, xlabel = "1/" + nameof(T_range), ylabel = nameof(na1, na2))
+
+# 7. График величины отклонения суммы зарядов от нуля (проверка условия электронейтральности) для каждой T
+Pr_T = fabs(p_T - n_T - na1 - na2)
+graph.print(T_range, Pr_T, log = True, xlabel = nameof(T_range), title = "Проверка условия электронейтральности")
